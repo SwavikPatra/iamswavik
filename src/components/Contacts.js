245813a7dbef5213b354a134dd45132484e538d5
swavik <kunuswavik@gmail.com>
2024-06-01 00:31:18 +0530

    Update image paths

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import "../css/contacts.css";
-// import phone from "/img/logo/phone.png"
-// import mail from "/img/logo/mail.png"
 
 function Contacts() {
   return (
     <div className="center-content">
       <div className="contact-item">
         <div className="contacts">
-          <img src ="${process.env.PUBLIC_URL}/img/logo/phone.png" className="contact-icon" alt= "phonee"/>
+          <img src ={`${process.env.PUBLIC_URL}/img/logo/phone.png`} className="contact-icon" alt= "phonee"/>
         </div>
         <h1>+91-9777853386</h1>
       </div>
@@ -18,7 +16,7 @@ function Contacts() {
       <div className="contact-item">
         <hr style={{ border: "1px solid ", margin: "20px 0px" }} />
         <div className="contacts mail">
-        <img src ="${process.env.PUBLIC_URL}/img/logo/mail.png" className="contact-icon" alt= "phonee"/>
+        <img src ={`${process.env.PUBLIC_URL}/img/logo/mail.png`} className="contact-icon" alt= "phonee"/>
         </div>
         <h1>kunuswavik@gmail.com</h1>
       </div>
--- src/components/FullstackProjects.js
+++ src/components/FullstackProjects.js
@@ -11,14 +11,14 @@ const projects = [
     details: "Designed and developed ’Online Market Place’, a user-friendly website for buying and selling Items.\nUser login system by combining the robust Django authentication system with the versatility of OAuth offering users a secure and streamlined login experience.\nSellers can add products with detailed descriptions, images, and prices. Buyers can easily browse products, search, contact to the seller to buy the product.\nBuyer and seller can chat seamlessly in real time. It uses Django channels which uses WebSockets, enabling users to engage in instantaneous and dynamic conversations.",
     githubLink: "https://github.com/SwavikPatra/online-market-place",
     images: [
-        "${process.env.PUBLIC_URL}/img/marketplace/s1.png",
-        "${process.env.PUBLIC_URL}/img/marketplace/s2.png",
-        "${process.env.PUBLIC_URL}/img/marketplace/s3.png",
-        "${process.env.PUBLIC_URL}/img/marketplace/s4.png",
-        "${process.env.PUBLIC_URL}/img/marketplace/s5.png",
-        "${process.env.PUBLIC_URL}/img/marketplace/s6.png",
-        "${process.env.PUBLIC_URL}/img/marketplace/s7.png",
-        "${process.env.PUBLIC_URL}/img/marketplace/s8.png"
+        `${process.env.PUBLIC_URL}/img/marketplace/s1.png`,
+        `${process.env.PUBLIC_URL}/img/marketplace/s2.png`,
+        `${process.env.PUBLIC_URL}/img/marketplace/s3.png`,
+        `${process.env.PUBLIC_URL}/img/marketplace/s4.png`,
+        `${process.env.PUBLIC_URL}/img/marketplace/s5.png`,
+        `${process.env.PUBLIC_URL}/img/marketplace/s6.png`,
+        `${process.env.PUBLIC_URL}/img/marketplace/s7.png`,
+        `${process.env.PUBLIC_URL}/img/marketplace/s8.png`
     ]
 },
 
@@ -29,9 +29,9 @@ const projects = [
     details: "A Twitter-like site for creating, deleting, and updating posts. Features include a like button, like count, and user registration/login, comments. Used Django Authentication system and for password reset Sendgrid API is used.",
     githubLink: "https://github.com/SwavikPatra/twitter-clone",
     images: [
-      "${process.env.PUBLIC_URL}/img/twitter/s1.png", // Example image paths
-      "${process.env.PUBLIC_URL}/img/twitter/s2.png",
-      "${process.env.PUBLIC_URL}/img/twitter/s3.png"
+      `${process.env.PUBLIC_URL}/img/twitter/s1.png`, // Example image paths
+      `${process.env.PUBLIC_URL}/img/twitter/s2.png`,
+      `${process.env.PUBLIC_URL}/img/twitter/s3.png`
     ]
   },
   // Add more projects here...
