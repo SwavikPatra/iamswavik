22197c748bc4e32dc568ae87f1082f771b6226cd
SwavikPatra <kunuswavik@gmail.com>
2024-09-23 23:42:09 +0530

    experience added

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -9,14 +9,13 @@ function Experiences() {
         <h3>Experience</h3>
         
         <div className="card">
-          <h5>Software Developer Intern (NodeJS, ReactJS)</h5>
-          <p className="p-x-small">Performalytic</p>
-          <p className="p-x-small">May 2023 - July 2023</p>
+          <h5>Software Developer Intern (Python, Flask, Fast API, Celery)</h5>
+          <p className="p-x-small">DataFoundary</p>
+          <p className="p-x-small">August 2024 - Present</p>
           <ul className="p-x-small">
-            <li>Implemented OAuth and JWT for secure user authentication and authorization in web applications, using bcrypt for password hashing to enhance security.</li>
-            <li>Integrated SendGrid API for password reset functionality, ensuring seamless user experience for account recovery.</li>
-            <li>Contributed to database architecture and implemented data management solutions using MySQL, performing queries and data manipulations to support application functionalities.</li>
-            <li>Utilized Git for version control to facilitate collaboration within the development team, ensuring smooth integration of code changes.</li>
+            <li>Built and developed backend APIs using <strong>Flask</strong> and integrated <strong>MongoDB</strong> for efficient audit management and data storage, handling sensitive medical field data.</li>
+            <li>Designed and implemented <strong>asynchronous APIs</strong> and background tasks using <strong>FastAPI</strong> and <strong>Celery</strong>, utilizing <strong>Redis</strong> as an in-memory database for improved performance and scalability.</li>
+            <li>Leveraged <strong>Git</strong> for version control and <strong>Docker</strong> for containerization to facilitate collaboration within the development team, ensuring smooth integration of code changes across <strong>14 team members</strong>.</li>
           </ul>
         </div>
         
@@ -24,6 +23,7 @@ function Experiences() {
       </div>
     </div>
   );
+  
 }
 
 export default Experiences;
